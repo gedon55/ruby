e4ed26e2e2df4626002ec040d818e10455b4dbc3
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-07-08 14:40:10 +0000

    finish2-11

---
+++ chapter11/lesson11.rb
@@ -0,0 +1,15 @@
+class Car
+ def run(a)
+   puts "車で#{a}キロ走ります"
+ end
+ end
+ 
+ class Truck<Car
+   def run(a)
+     super
+     puts "大きな荷物を乗せて走ります。"
+   end
+ end
+ 
+ truck=Truck.new
+ truck.run(5)
--- chapter11/override.rb
+++ chapter11/override.rb
@@ -1,15 +1,20 @@
 class Car
- def run(distance)
+  def run(distance)
    puts "車で#{distance}キロ走ります。"
- end
+  end
  end
  
  class Bus<Car
    def run(distance)
-     puts "バスで#{distance}キロ走ります。"
+     super
+     puts "３０人を乗せて、走っています。"
    end
  end
  
  bus=Bus.new
  bus.run(5)
+ 
+ car=Car.new
+ car.run(5)
+ 
  
